918f01d837b4e0796d0cfa68cd65df96765701b0
FrankYork <frankgv.2015@gmail.com>
2016-02-29 18:06:34 -0500

    Frank York Garay

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-index.php
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Code York</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+<center>
+	<img src="1.jpg" alt="">
+	<img src="1.jpg" alt="">
+</center>
+<marquee>
+	<h1>Frank York Garay Vela</h1>
+	</marquee>
+</body>
+</html>
